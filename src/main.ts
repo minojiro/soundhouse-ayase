54dd9baf1387224a15bbe79647374a8c5a3c42ba
minojiro <minojiro@users.noreply.github.com>
2022-03-13 02:43:43 +0900

    refactor: remove unused codes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import "./index.css";
 
 const APP_ID = "soundhouse-ayase";
 if (!document.getElementById(APP_ID)) {
-  console.log(APP_ID);
   const el = document.createElement("div");
   el.id = APP_ID;
   document.body.appendChild(el);
